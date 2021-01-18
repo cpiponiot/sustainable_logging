ad067c567d922bd0f1c498442c1c3495e35ff621
Camille Piponiot <camille.piponiot@gmail.com>
2021-01-18 13:19:53 +0100

    add test file for the simulateLogging function (this file could be changed later when this repo is made into a package)

---
+++ test_file.R
@@ -0,0 +1,121 @@
+# test new method
+
+source("R/simulateLogging.R")
+
+
+## test that simulateLogging works with one set of inputs
+timeLength = 100
+logIntensity = 20
+logCycle = 35
+omega0 = 0.5
+longitude = -60.6
+latitude = -3.3
+dti = NA
+uncertainties = TRUE
+area = NA
+silv = c(0,0)
+keepAll = FALSE
+
+result = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+testthat::expect_equal(nrow(result), ceiling(timeLength/logCycle)*(2*2 + 1))
+
+## test that simulateLogging works when uncertainties = FALSE
+timeLength = 100
+logIntensity = 20
+logCycle = 35
+omega0 = 0.5
+longitude = -60.6
+latitude = -3.3
+dti = NA
+uncertainties = FALSE
+area = NA
+silv = c(0,0)
+keepAll = FALSE
+result2 = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+testthat::expect_equal(nrow(result2), ceiling(timeLength/logCycle)*(2*2 + 1))
+
+
+## test that simulateLogging works when keepAll = TRUE
+timeLength = 100
+logIntensity = 20
+logCycle = 35
+omega0 = 0.5
+longitude = -60.6
+latitude = -3.3
+dti = NA
+uncertainties = TRUE
+area = NA
+silv = c(0,0)
+keepAll = TRUE
+
+result3 = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+testthat::expect_equal(nrow(result3), ceiling(timeLength/logCycle)*(2*logCycle + 1))
+
+## test that simulateLogging works with multiple sets of inputs
+timeLength = 100
+logIntensity = rep(10, 4); logIntensity[2] = 30
+logCycle = rep(35, 4); logCycle[3] = 65
+omega0 = rep(0.90, 4); omega0[4] = 0.30
+longitude = rep(-60.6, 4)
+latitude = rep(-3.3, 4)
+dti = NA
+uncertainties = TRUE
+area = NA
+silv = c(0,0)
+keepAll = FALSE
+
+result4 = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+testthat::expect_equal(nrow(result4), max(ceiling(timeLength/logCycle))*length(logCycle)*(2*2 + 1))
+
+## test that simulateLogging works with multiple sets of inputs and keepAll = TRUE
+timeLength = 100
+logIntensity = rep(10, 4); logIntensity[2] = 30
+logCycle = rep(35, 4); logCycle[3] = 65
+omega0 = rep(0.90, 4); omega0[4] = 0.30
+longitude = rep(-60.6, 4)
+latitude = rep(-3.3, 4)
+dti = NA
+uncertainties = TRUE
+area = NA
+silv = c(0,0)
+keepAll = TRUE
+
+result5 = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+testthat::expect_equal(nrow(result5), max(ceiling(timeLength/logCycle))*sum(2*logCycle + 1))
+
+
+## test that simulateLogging works with multiple sets of inputs and uncertainties = FALSE
+timeLength = 100
+logIntensity = rep(10, 4); logIntensity[2] = 30
+logCycle = rep(35, 4); logCycle[3] = 65
+omega0 = rep(0.90, 4); omega0[4] = 0.30
+longitude = rep(-60.6, 4)
+latitude = rep(-3.3, 4)
+dti = NA
+uncertainties = FALSE
+area = NA
+silv = c(0,0)
+keepAll = FALSE
+
+result6 = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+testthat::expect_equal(nrow(result6), max(ceiling(timeLength/logCycle))*length(logCycle)*(2*2 + 1))
+
+## test that simulateLogging works with multiple sets of inputs and uncertainties = FALSE and keepAll = TRUE
+timeLength = 100
+logIntensity = rep(10, 4); logIntensity[2] = 30
+logCycle = rep(35, 4); logCycle[3] = 65
+omega0 = rep(0.90, 4); omega0[4] = 0.30
+longitude = rep(-60.6, 4)
+latitude = rep(-3.3, 4)
+dti = NA
+uncertainties = FALSE
+area = NA
+silv = c(0,0)
+keepAll = TRUE
+
+result7 = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+testthat::expect_equal(nrow(result7), max(ceiling(timeLength/logCycle))*sum(2*logCycle + 1))
+
+
+## test that we get same results with analystical solution used in `timbRecovery()`
+
