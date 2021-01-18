426da3065b4bd1c08f9453f317fffed220a227fa
Camille Piponiot <camille.piponiot@gmail.com>
2021-01-18 18:29:05 +0100

    testing whether the 2 approachs give the same results

--- test_file.R
+++ test_file.R
@@ -118,4 +118,28 @@ testthat::expect_equal(nrow(result7), max(ceiling(timeLength/logCycle))*sum(2*lo
 
 
 ## test that we get same results with analystical solution used in `timbRecovery()`
+timeLength = 100
+logIntensity = 20
+logCycle = 35
+omega0 = 0.5
+longitude = -60.6
+latitude = -3.3
+dti = NA
+uncertainties = TRUE
+area = NA
+silv = c(0,0)
+keepAll = TRUE
+
+result = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+resultbis = timbRecovery(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area)
+vcomm1 = result$med[result$variable=="volume"][1:100]*result$med[result$variable=="omega"][1:100]
+vcomm2 = resultbis[[1]]$med[2:101]
+
+testthat::expect_true(all(abs(vcomm1-vcomm2) < 2))
+
+plot(y = vcomm1,x = 1:100, 
+     xlab = "time", ylab = "Commercial volume", 
+     type = "l", ylim = c(0,40), col = 2) 
+lines(x = 1:100, y = vcomm2)
+
 
