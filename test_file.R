fe172444984e55fefe77899f81d8af5c2f07a716
Camille Piponiot <camille.piponiot@gmail.com>
2021-01-19 12:24:27 +0100

    test that previous and new versions give the same results (for now when pdef = 0)

--- test_file.R
+++ test_file.R
@@ -1,6 +1,7 @@
 # test new method
 
-source("R/simulateLogging.R")
+file.sources = list.files(path = "R/", pattern="*.R", full.names = TRUE)
+sapply(file.sources, source, .GlobalEnv)
 
 
 ## test that simulateLogging works with one set of inputs
@@ -125,21 +126,43 @@ omega0 = 0.5
 longitude = -60.6
 latitude = -3.3
 dti = NA
-uncertainties = TRUE
+uncertainties = FALSE
 area = NA
 silv = c(0,0)
 keepAll = TRUE
 
 result = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
 resultbis = timbRecovery(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area)
-vcomm1 = result$med[result$variable=="volume"][1:100]*result$med[result$variable=="omega"][1:100]
-vcomm2 = resultbis[[1]]$med[2:101]
+vcomm1 = result$value[result$variable=="volume"][1:100]*result$value[result$variable=="omega"][1:100]
+vcomm2 = resultbis[[1]]$volTimb[2:101]
 
-testthat::expect_true(all(abs(vcomm1-vcomm2) < 2))
+testthat::expect_true(all(abs(vcomm1-vcomm2) < 5))
+testthat::expect_true(all(abs(vcomm1-vcomm2) < 0.5+1/50*1:length(vcomm1)))
 
 plot(y = vcomm1,x = 1:100, 
      xlab = "time", ylab = "Commercial volume", 
-     type = "l", ylim = c(0,40), col = 2) 
+     type = "l", ylim = c(0,80), col = 2) 
 lines(x = 1:100, y = vcomm2)
 
+## with uncertainties
+uncertainties = TRUE
+result = simulateLogging(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area, silv, keepAll) 
+resultbis = timbRecovery(timeLength, logIntensity, logCycle, omega0, longitude, latitude,  dti, uncertainties, area)
 
+vcomm1 = result$med[result$variable=="volume"][1:100]*result$med[result$variable=="omega"][1:100]
+vinf1 = result$lwr[result$variable=="volume"][1:100]*result$lwr[result$variable=="omega"][1:100]
+vsup1 = result$upr[result$variable=="volume"][1:100]*result$upr[result$variable=="omega"][1:100]
+
+vcomm2 = resultbis[[1]]$med[2:101]
+vinf2 = resultbis[[1]]$inf[2:101]
+vsup2 = resultbis[[1]]$sup[2:101]
+
+testthat::expect_true(all(vcomm1 > vinf2 & vcomm1 < vsup2))
+testthat::expect_true(all(vcomm2 > vinf1 & vcomm2 < vsup1))
+
+plot(y = vcomm1,x = 1:100, 
+     xlab = "time", ylab = "Commercial volume", 
+     type = "l", ylim = c(0,80), col = 2) 
+lines(x = 1:100, y = vcomm2)
+polygon(x = c(1:100, 100:1), y = c(vinf1, rev(vsup1)), col = "#00000020", border = NA)
+polygon(x = c(1:100, 100:1), y = c(vinf2, rev(vsup2)), col = "#FF000020", border = NA)
